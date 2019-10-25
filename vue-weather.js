4830a03ff9b250f8c2a9b80cf5a49f177ceed48e
Aaron I Trierweiler <ait530@users.noreply.github.com>
2019-10-25 18:22:35 -0500

    Change to async mounted function to make request to openweathermap api

--- vue-weather.js
+++ vue-weather.js
@@ -1,16 +1,19 @@
-console.log('Script connected.')
-import config from './config.js';
-
-new Vue({
-    el: '#display-weather',
-    data () {
-      return {
-        info: null
-      }
-    },
-    mounted () {
-      axios
-        .get(`http://api.openweathermap.org/data/2.5/weather?zip=60202,us&APPID=${config.apiKey}`)
-        .then(response => (this.info = response))
+var app = new Vue({
+  el: '#app',
+  data () {
+    return {
+      weather: null
     }
+  },
+  async mounted () {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Evanston&APPID=${config.apiKey}`);
+    const data = await response.json();
+    this.weather = data.main.temp;
+  },
+  template: `
+  <div>
+    Here's the weather in Evanston today:
+    <h1>{{ weather }}</h1>
+  </div>
+    `
 })
