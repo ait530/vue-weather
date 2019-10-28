efc772ae57d6c998e73677198b02149f246aa305
Aaron I Trierweiler <ait530@users.noreply.github.com>
2019-10-28 17:15:39 -0500

    Add msg input and form from handling forms documentation: https://012.vuejs.org/guide/forms.html

--- vue-weather.js
+++ vue-weather.js
@@ -1,12 +1,14 @@
 var app = new Vue({
   el: '#app',
+
   data () {
     return {
-      weather: null
+      weather: null,
+      msg      : 'hello, world!'
     }
   },
   async mounted () {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Evanston&APPID=${config.apiKey}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=60202,us&APPID=${config.apiKey}`);
     const data = await response.json();
     this.weather = data.main.temp;
   },
@@ -14,6 +16,15 @@ var app = new Vue({
   <div>
     Here's the weather in Evanston today:
     <h1>{{ weather }}</h1>
+
+    <form id="demo">
+      <!-- text -->
+      <p>
+        <input type="text" v-model="msg">
+        {{msg}}
+      </p>
+
+    </form>
   </div>
     `
 })
