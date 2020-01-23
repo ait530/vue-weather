e893efc9c613abf1e479c4b6eeb0e51ecb3f6672
Aaron I Trierweiler <ait530@users.noreply.github.com>
2020-01-23 12:03:23 -0600

    Add feelsLike temp; remove fahrenheit in data(), methods(), mounted(); add units=imperial to api fetch

--- vue-weather.js
+++ vue-weather.js
@@ -5,42 +5,41 @@ var app = new Vue({
   data () {
     return {
       weather: null,
-      fahrenheit: null,
       city: null,
-      description: null
+      description: null,
+      feelsLike: null
     }
   },
   methods: {
     requestWeather: async function (e) {
       var userInput = document.getElementById("zipcodeInput").value;
-      const response = await fetch("http://api.openweathermap.org/data/2.5/weather?zip=" + userInput + ",us&APPID=" + apikey);
+      const response = await fetch("http://api.openweathermap.org/data/2.5/weather?zip=" + userInput + ",us&APPID=" + apikey + "&units=imperial");
       const data = await response.json();
       this.weather = data.main.temp;
       this.city = data.name;
-      this.fahrenheit = Math.round(((((this.weather - 273.15) * 9) / 5) + 32));
       this.description = data.weather[0].description;
-      console.log(data);
+      this.feelsLike = data.main.feels_like;
 
     }
   },
   async mounted () {
-    const response = await fetch("http://api.openweathermap.org/data/2.5/weather?zip=60202,us&APPID=" + apikey);
+    const response = await fetch("http://api.openweathermap.org/data/2.5/weather?zip=60202,us&APPID=" + apikey + "&units=imperial");
     const data = await response.json();
     this.weather = data.main.temp;
     this.city = data.name;
-    this.fahrenheit = Math.round((((this.weather - 273.15) * 9) / 5) + 32);
     this.description = data.weather[0].description;
+    this.feelsLike = data.main.feels_like;
 
-    
+  
   },
   template: `
   <div>
     Here's the weather in {{ city }} today:
-    <h1>{{ weather }}</h1>
-
-    <h2>{{ fahrenheit }} &ordm;F</h2>
+    <h1>{{ weather }} &ordm;F</h1>
 
     <h2>{{ description }}</h2>
+
+    <h2> Feels like: {{ feelsLike }} &ordm;F</h2>
 
     <form id="demo" v-on:submit.prevent="requestWeather">
       <!-- text -->
